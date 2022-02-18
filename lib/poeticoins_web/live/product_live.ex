2a43c218aa3c5f065a9986c16b81ecb1e28c7408
Guilherme Pressutto <gpressutto5@gmail.com>
2022-02-18 00:00:43 -0300

    Added prices table to product page

--- lib/poeticoins_web/live/product_live.ex
+++ lib/poeticoins_web/live/product_live.ex
@@ -5,12 +5,14 @@ defmodule PoeticoinsWeb.ProductLive do
   def mount(%{"id" => product_id} = _params, _session, socket) do
     product = product_from_string(product_id)
     trade = Poeticoins.get_last_trade(product)
+    trades = get_trade_history()
 
     socket =
       assign(socket,
         product: product,
         product_id: product_id,
         trade: trade,
+        trades: trades,
         page_title: page_title_from_trade(trade)
       )
 
@@ -18,24 +20,43 @@ defmodule PoeticoinsWeb.ProductLive do
       Poeticoins.subscribe_to_trades(product)
     end
 
-    {:ok, socket}
+    {:ok, socket, temporary_assigns: [trades: []]}
   end
 
   def render(%{trade: trade} = assigns) when not is_nil(trade) do
     ~L"""
     <div class="row">
       <div class="column"
-          phx-hook="StockChart"
-          phx-update="ignore"
-          id="product-chart"
-          data-product-id="<%= to_string(@product) %>"
-          data-product-name="<%= @product.exchange_name %> <%= @product.currency_pair %>"
-          data-trade-timestamp="<%= DateTime.to_unix(@trade.traded_at, :millisecond) %>"
-          data-trade-volume="<%= @trade.volume %>"
-          data-trade-price="<%= @trade.price %>"
-      >
+              phx-hook="StockChart"
+              phx-update="ignore"
+              id="product-chart"
+              data-product-id="<%= to_string(@product) %>"
+              data-product-name="<%= @product.exchange_name %> <%= @product.currency_pair %>"
+              data-trade-timestamp="<%= DateTime.to_unix(@trade.traded_at, :millisecond) %>"
+              data-trade-volume="<%= @trade.volume %>"
+              data-trade-price="<%= @trade.price %>"
+        >
         <div id="stockchart-container"></div>
       </div>
+      <div class="column">
+        <table id="trade-history">
+          <thead>
+            <th>Time</th>
+            <th>Price</th>
+            <th>Volume</th>
+          </thead>
+          <tbody phx-update="prepend" phx-hook="TradeHistory" id="trade-history-rows">
+            <%= for trade <- @trades do %>
+              <tr id="trade-<%= timestamp(trade.traded_at) %>">
+                <td><%= trade.traded_at %></td>
+                <td><%= trade.price %></td>
+                <td><%= trade.volume %></td>
+              </tr>
+            <% end %>
+          </tbody>
+        </table>
+      </div>
+    </div>
     """
   end
 
@@ -52,6 +73,7 @@ defmodule PoeticoinsWeb.ProductLive do
       socket
       |> assign(:trade, trade)
       |> assign(:page_title, page_title_from_trade(trade))
+      |> update(:trades, &[trade | &1])
 
     {:noreply, socket}
   end
@@ -59,5 +81,13 @@ defmodule PoeticoinsWeb.ProductLive do
   defp page_title_from_trade(trade) do
     "#{fiat_character(trade.product)}#{trade.price}" <>
       " #{trade.product.currency_pair} #{trade.product.exchange_name}"
+  end
+
+  defp timestamp(dt) do
+    DateTime.to_unix(dt, :millisecond)
+  end
+
+  defp get_trade_history do
+    []
   end
 end
